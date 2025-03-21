c96caec0dc661c58160bcae88857ed362de45b00
eduardoseijics <eduardoseiji202@gmail.com>
2025-03-20 23:17:27 -0300

    add next seven days forecast

---
+++ app/Services/OpenMeteo/OpenMeteoApi.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Services\OpenMeteo;
+
+use RuntimeException;
+use App\Facades\ApiService;
+use App\Facades\OpenMeteoApiFacade;
+
+/**
+ * 
+ * @author Eduardo Seiji
+ */
+class OpenMeteoApi {
+  /**
+   * Makes the request to get weather data based on the query string.
+   *
+   * @param string $queryString
+   * @return array
+   * @throws RuntimeException
+   */
+  public static function fetchWeatherData($queryString): array
+  {
+    $response = OpenMeteoApiFacade::get('?' . $queryString)->json();
+
+    if (empty($response) || !isset($response)) {
+      throw new RuntimeException('Houve um erro na requisição, tente novamente mais tarde');
+    }
+
+    return $response;
+  }
+}
